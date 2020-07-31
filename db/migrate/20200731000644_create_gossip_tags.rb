69e5e963a03b4ef744ec89b02b964390cf08a4e3
Mocadi-dot <59927623+mocadi-dot@users.noreply.github.com>
2020-07-31 08:33:13 +0200

    maj

--- db/migrate/20200731000644_create_gossip_tags.rb
+++ db/migrate/20200731000644_create_gossip_tags.rb
@@ -1,7 +1,8 @@
-class CreateGossipTags < ActiveRecord::Migration[5.2]
+class CreateGossipsTags < ActiveRecord::Migration[5.2]
   def change
-    create_table :gossip_tags do |t|
-
+    create_table :gossips_tags do |t|
+      t.belongs_to :gossip, index: true
+      t.belongs_to :tag, index: true
       t.timestamps
     end
   end
--- db/migrate/20200731000823_create_private_messages.rb
+++ db/migrate/20200731000823_create_private_messages.rb
@@ -1,7 +1,9 @@
 class CreatePrivateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :private_messages do |t|
-
+      t.string :content
+      t.references :recipient, index: true
+      t.references :sender, index: true
       t.timestamps
     end
   end
